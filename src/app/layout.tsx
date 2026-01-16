52af92ba353b3da18eedf9917f4ab5fe4ae9df83
Hasan Qadri <haesanq@yahoo.com>
2026-01-15 19:50:11 -0500

    Updated website metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,28 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+const metadataBase = new URL('https://laylight.us') // Replace with your domain
+
 export const metadata: Metadata = {
+    metadataBase: metadataBase,
     title: "Laylight",
     description: "Your personal job coach",
+    openGraph: {
+        title: "Laylight Career Consulting",
+        description: "Navigate your next big move. Your path to the top, illuminated.",
+        url: "https://laylight.com",
+        siteName: "Laylight",
+        images: [
+            {
+                url: "https://laylight.com/images/share-preview.png", // Must be an absolute URL
+                width: 1200,
+                height: 630,
+                alt: "Laylight Career Consulting Preview",
+            },
+        ],
+        locale: "en_US",
+        type: "website",
+    },
 };
 
 export const dynamic = "force-dynamic";
